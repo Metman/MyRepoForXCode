74501e4325f281e77338fa9494b3e470a3189435
Максим Филимонов <metallomaniac666@me.com>
2016-09-28 19:37:16 +0300

    First

---
+++ NewsListTableViewController.h
@@ -0,0 +1,18 @@
+//
+//  TableViewController.h
+//  News
+//
+//  Created by Максим Филимонов on 21.09.16.
+//  Copyright © 2016 Максим Филимонов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "NewsItem.h"
+#import "NewsListDataSource.h"
+#import "NewsListTableViewCell.h"
+
+@interface NewsListTableViewController : UITableViewController
+
+@property(nonatomic,strong) NewsListDataSource* newsListDatasourceDelegate;
+
+@end
