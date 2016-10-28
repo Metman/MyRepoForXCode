cc59f37136f73a10fd9f8cf947c2a5fb5364c90c
Максим Филимонов <metallomaniac666@me.com>
2016-10-28 11:28:02 +0300

    Bla bla bla

--- News/NewsListDataSource.m
+++ News/NewsListDataSource.m
@@ -118,6 +118,15 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     
+    
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT,0), ^{
+        for(int i = 0; i < 5000; i++){
+        NSLog(@"%d", i);
+    };
+    
+    });
+    
     UIStoryboard * storyboard = [UIStoryboard storyboardWithName:@"Main"
                                                           bundle:nil];
     
